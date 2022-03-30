5f6d3efd516b9a57669e459a345e7956dd13fcc4
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-03-30 22:32:33 +0100

    Update install.sh

--- scripts/docker/install.sh
+++ scripts/docker/install.sh
@@ -19,4 +19,5 @@ sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 
 # Install yacht.sh
 docker volume create yacht
+docker pull selfhostedpro/yacht
 docker run -d -p 8001:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
